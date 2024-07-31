883634d5f3ec77ffe4f6f833d37d49311ddbe332
devopseasylearning <info@devopseasylearning.com>
2024-07-31 14:23:15 -0400

    changes

--- First-steps-installation-configuration.groovy
+++ First-steps-installation-configuration.groovy
@@ -69,3 +69,61 @@ containing the Prometheus binary and run:
 Prometheus should start up. You should also be able to browse to a status page about itself 
 at http://localhost:9090. 
 Give it about 30 seconds to collect data about itself from its own HTTP metrics endpoint.
+
+You can also verify that Prometheus is serving metrics about itself 
+by navigating to its own metrics endpoint: http://localhost:9090/metrics.
+
+4. Using The Expression Browser:
+
+Let us try looking at some data that Prometheus has collected about itself. 
+To use Prometheus built-in expression browser, navigate to http://localhost:9090/graph 
+and choose the "Table" view within the "Graph" tab.
+
+As you can gather from http://localhost:9090/metrics, 
+one metric that Prometheus exports about itself is called 
+'promhttp_metric_handler_requests_total' (the total number of /metrics requests the Prometheus 
+                                          ----------------------------
+server has served). 
+Go ahead and enter this into the Expression Console:
+'promhttp_metric_handler_requests_total'
+This should return a number of different time series (along with the latest value recorded for 
+each), all with the metric name promhttp_metric_handler_requests_total, but with different 
+labels. These labels designate different requests statuses.
+
+Note:
+If we were only interested in requests that resulted in HTTP code 200, we could use this 
+query to retrieve that information:
+' promhttp_metric_handler_requests_total{code="200"} '
+
+To count the number of returned time series, you could write:
+   ----------------------------------------
+
+' count(promhttp_metric_handler_requests_total) '
+
+
+5. Using The Graphing Interface:
+
+To graph expressions, navigate to http://localhost:9090/graph and use the "Graph" tab.
+
+For example, enter the following expression to graph the per-second HTTP request rate returning
+status code 200 happening in the self-scraped Prometheus:
+ ' rate(promhttp_metric_handler_requests_total{code="200"}[1m]) '
+
+You can experiment with the graph range parameters and other settings.
+
+
+6.Monitoring Other Targets:
+
+Collecting metrics from Prometheus alone is not a great representation of 
+Prometheus capabilities. To get a better sense of what Prometheus can do, 
+we recommend exploring documentation about other exporters. 
+The Monitoring Linux or macOS host metrics using a node exporter guide is a good place to start.
+
+https://prometheus.io/docs/guides/node-exporter/
+
+Summary
+-------
+In this guide, you installed Prometheus, configured a Prometheus instance to monitor resources,
+ and learned some basics of working with time series data in Prometheus expression browser.
+To continue learning about Prometheus, check out the Overview for some ideas about what to 
+explore next.
