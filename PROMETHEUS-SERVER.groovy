704339f2a5361e02de5e67c35d2d42d379c56a5d
devopseasylearning <info@devopseasylearning.com>
2024-08-09 23:35:07 -0400

    changes

---
+++ PROMETHEUS-SERVER.groovy
@@ -0,0 +1,20 @@
+
+I.Getting Started: 
+
+This Guide is a "Hello World"-Style Tutorial which shows how to 'install, configure, and use a
+ simple Prometheus instance.' You will 'download and run Prometheus locally', 'configure it to 
+ scrape itself and an example application, 'then work with queries', 'rules, and 'graphs to use 
+ collected time series data.'
+
+1.Downloading And Running Prometheus;
+
+Download the latest release of Prometheus for your platform, then extract and run it:
+
+Open to check and Download:
+https://prometheus.io/download/ ( download what suit your need: linux, macos,etc...)
+
+' tar xvfz prometheus-*.tar.gz
+  cd prometheus-*  '
+
+2. Configuring Prometheus 'to monitor itself'
+
